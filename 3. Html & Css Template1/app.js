f8c515186d4feea04c3617713bb4b3fa069b04fe
Mohamed Radwan <101032571+mo0hamedRadwan@users.noreply.github.com>
2023-04-25 01:01:35 +0200

    Add files via upload

---
+++ 3. Html & Css Template1/app.js
@@ -0,0 +1,3 @@
+document.querySelector('.icon').addEventListener('click' , ()=>{
+    document.querySelector('.links ul').classList.toggle('showMenu');
+})
---
+++ 4. Html & Css Template2/app.js
@@ -0,0 +1,3 @@
+document.querySelector(".toggle-menu").addEventListener('click' , ()=>{
+    document.querySelector("nav ul").classList.toggle("showMenu");
+})
